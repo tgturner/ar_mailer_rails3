894d1ef3ecdb097623f820ec4aade474caa50b5f
Graham Turner <turnertgraham@gmail.com>
2017-03-20 22:26:00 -0400

    updates to rails 5

--- lib/ar_mailer_rails3/ar_sendmail.rb
+++ lib/ar_mailer_rails3/ar_sendmail.rb
@@ -383,8 +383,9 @@ class ArMailerRails3::ARSendmail
     log "Starting SMTP"
     smtp.start(*settings) do |session|
       @failed_auth_count = 0
-      until emails.empty? do
-        email = emails.to_a.shift
+      instantiated_emails = emails.to_a
+      until instantiated_emails.empty? do
+        email = instantiated_emails.shift
         begin
           log "about to send email %011d from %s to %s" % [email.id, email.from, email.to]
           res = session.send_message email.mail, email.from, email.to
